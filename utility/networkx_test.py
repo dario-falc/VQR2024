99f49903aeca470e17a5eb19c42fdd59fa3192fe
Dar7oo <143949654+dario-falc@users.noreply.github.com>
2025-11-04 20:09:23 +0100

    Implemented VQR model with graph

--- utility/networkx_test.py
+++ utility/networkx_test.py
@@ -1,10 +1,16 @@
 import networkx as nx
 from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
+import numpy as np
 
 GENERATION_PARAMETERS = {}
-file_path = "data/VQR_50_200_0.30_0_10_4070.dat"
+file_path = "../data/VQR_50_200_0.30_0_10_4070.dat"
+
 G = nx.Graph()
+GRAPH_DATA = {}
+GRAPH_DATA["researchers"] = []
+GRAPH_DATA["papers"] = []
+GRAPH_DATA["scored_edges"] = {}
 
 with open(file_path, "r") as f:
     cur_line=1
@@ -15,37 +21,34 @@ with open(file_path, "r") as f:
         
         if cur_line <= 10:
             k, v = l.split(": ")
-            GENERATION_PARAMETERS[k] = float(v)            
+            GENERATION_PARAMETERS[k] = float(v) if k == "RDensity" else int(v)           
             cur_line+=1
             continue
-        
-        # Start papers count from #researchers+1 so node names don't overlap between the two subsets
-        n_r = GENERATION_PARAMETERS["Researcher"]
-        
+                
         # Split file row into researcher, paper and score
         r, p, s = l.split()
-        # r = int(r)
-        p = str(int(int(p) + n_r + 1))
+        r = "R" + r
+        p = "P" + p
         s = float(s)
         
-        
-        ## Add nodes r and p, and edge (r, p) with score s
-        # bipartite = 0 indicates that the node belongs to the researchers set
-        # bipartite = 1 indicates that the node belongs to the papers set
-        G.add_node(r, bipartite = 0)
-        G.add_node(p, bipartite = 1)        
-        G.add_edge(r, p, weight = s)
-        # print(f"{cur_line} - Adding edge ({r}, {p}) with score {s}")
-        # cur_line+=1
-        
+        GRAPH_DATA["researchers"].append(r) if r not in GRAPH_DATA["researchers"] else None
+        GRAPH_DATA["papers"].append(p) if p not in GRAPH_DATA["papers"] else None
+        GRAPH_DATA["scored_edges"][(r, p)] = s
+
+print(f"GRAPH_DATA: {GRAPH_DATA}")
+
+G.add_nodes_from(GRAPH_DATA["researchers"], bipartite = 0)      # bipartite = 0 are the researchers
+G.add_nodes_from(GRAPH_DATA["papers"], bipartite = 1)           # bipartite = 1 are the papers
+
+for edge, score in GRAPH_DATA["scored_edges"].items():
+    G.add_edge(edge[0], edge[1], weight = score)
+
 
 print(G)
-# print(nx.is_bipartite(G))
+print(nx.is_bipartite(G))
 # researchers, papers = bipartite.sets(G)
 
-
-
 ## Draw graph 
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.show()
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
 
--- vqr_model.py
+++ vqr_model.py
@@ -1,10 +1,14 @@
 from docplex.mp.model import Model
 import networkx as nx
-import numpy as np
 
 def init_graph(file_path):
     GENERATION_PARAMETERS = {}
+
     G = nx.Graph()
+    GRAPH_DATA = {}
+    GRAPH_DATA["researchers"] = []
+    GRAPH_DATA["papers"] = []
+    GRAPH_DATA["scored_edges"] = {}
 
     with open(file_path, "r") as f:
         cur_line=1
@@ -15,67 +19,71 @@ def init_graph(file_path):
             
             if cur_line <= 10:
                 k, v = l.split(": ")
-                GENERATION_PARAMETERS[k] = float(v)            
+                GENERATION_PARAMETERS[k] = float(v) if k == "RDensity" else int(v)           
                 cur_line+=1
                 continue
-            
-            # Start papers count from #researchers+1 so node names don't overlap between the two subsets
-            n_r = GENERATION_PARAMETERS["Researcher"]
-            
+                    
             # Split file row into researcher, paper and score
             r, p, s = l.split()
-
-            p = str(int(int(p) + n_r + 1))
-            s = float(s)            
+            r = "R" + r
+            p = "P" + p
+            s = float(s)
             
-            ## Add nodes r and p, and edge (r, p) with score s
-            # bipartite = 0 indicates that the node belongs to the researchers set
-            # bipartite = 1 indicates that the node belongs to the papers set
-            G.add_node(r, bipartite = 0)
-            G.add_node(p, bipartite = 1)        
-            G.add_edge(r, p, weight = s)
-            
-    return G, GENERATION_PARAMETERS
+            GRAPH_DATA["researchers"].append(r) if r not in GRAPH_DATA["researchers"] else None
+            GRAPH_DATA["papers"].append(p) if p not in GRAPH_DATA["papers"] else None
+            GRAPH_DATA["scored_edges"][(r, p)] = s
+
+    G.add_nodes_from(GRAPH_DATA["researchers"], bipartite = 0)      # bipartite = 0 are the researchers
+    G.add_nodes_from(GRAPH_DATA["papers"], bipartite = 1)           # bipartite = 1 are the papers
+
+    for edge, score in GRAPH_DATA["scored_edges"].items():
+        G.add_edge(edge[0], edge[1], weight = score)
+
+        
+    return G, GRAPH_DATA, GENERATION_PARAMETERS
 
 
-def main():
-    file_path = "data/VQR_50_200_0.30_0_10_4070.dat"
-    G, GENERATION_PARAMETERS = init_graph(file_path)
-    
+def solve_instance(G, GRAPH_DATA, GENERATION_PARAMETERS):
     n_r = GENERATION_PARAMETERS["Researcher"]
     n_p = GENERATION_PARAMETERS["Papers"]
     
     ## Modello
     vqr_model = Model("VQR assignment")
     
+    ## Liste di ricercatori e papers
+    researchers = ["R" + str(i) for i in range(n_r)]
+    papers = ["P" + str(i) for i in range(n_p)]
+    
+    ## Dizionario con matrice di adiacenza con scores
+    scores = GRAPH_DATA["scored_edges"]
+    
     ## Variabili
-    x = vqr_model.binary_var_matrix(n_r,
-                                    n_p,
-                                    name="x")
+    # x = vqr_model.binary_var_list(G.edges, name="x")
+    x = vqr_model.binary_var_dict(G.edges, name="x")
 
     ## Vincoli
-    # Ad ogni soggetto deve essere assegnato almeno 1 prodotto
-    vqr_model.add_constraints((sum(x[i,j] for i in range(n_p)) >= 1
-                               for j in range(n_r)),
-                              names="min_papers_per_researcher")
+    # Ad ogni ricercatore deve essere assegnato almeno 1 paper
+    vqr_model.add_constraints((sum(x.get((r, p), 0) for p in papers) >= 1
+                               for r in researchers),
+                              names = "min_papers_per_researcher")
+    
+    # Ad ogni ricercatore possono essere assegnati massimo 4 paper
+    vqr_model.add_constraints((sum(x.get((r, p), 0) for p in papers) <= 4
+                               for r in researchers),
+                              names = "max_papers_per_researcher")
 
-    # Ad ogni soggetto possono essere assegnati massimo 4 prodotti
-    vqr_model.add_constraints((sum(x[i,j] for i in range(n_p)) <= 4
-                               for j in range(n_r)),
-                              names="max_papers_per_researcher")
-
-    # Ogni prodotto può essere assegnato massimo ad solo soggetto
-    vqr_model.add_constraints((sum(x[i,j] for j in range(n_r)) <= 1
-                               for i in range(n_p)),
+    # Ogni paper può essere assegnato massimo ad solo ricercatore
+    vqr_model.add_constraints((sum(x.get((r, p), 0) for r in researchers) <= 1
+                               for p in papers),
                               names="max_researchers_per_paper")
 
     # Il dipartimento deve fornire al più 2.5*n_r paper
     alpha = 2.5 * n_r
-    vqr_model.add_constraint((sum(x[i,j] for i in range(n_p) for j in range(n_r)) == alpha))
+    vqr_model.add_constraint((sum(x.get((r, p), 0) for r in researchers for p in papers) == alpha))
 
 
     ## Funzione obiettivo
-    obj_fn = sum(s[i,j] * x[i,j] for i in range(n_p) for j in range(n_r))
+    obj_fn = sum(scores.get((r, p), 0) * x.get((r, p), 0) for r in researchers for p in papers)
     vqr_model.set_objective("max", obj_fn)
 
     vqr_model.print_information()
@@ -87,6 +95,15 @@ def main():
     print(vqr_model.solve_details)
     
     vqr_model.print_solution()
+
+
+def main():
+    file_path = "data/VQR_50_200_0.30_0_10_4070.dat"
+    G, SCORED_EDGES, GENERATION_PARAMETERS = init_graph(file_path)
+    
+    solve_instance(G, SCORED_EDGES, GENERATION_PARAMETERS)
+
+
 
 if __name__ == "__main__":
     main()
